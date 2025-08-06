ed1e5273418fe99edf3c611995f2f360e881c405
Rafael de Oliveira <rafael.marques@aluno.ifce.edu.br>
2025-08-06 17:48:21 -0300

    Add files via upload

---
+++ Lista_Exercicios_1/questao01.py
@@ -0,0 +1,5 @@
+##O volume de uma esfera com raio r é dado por 
+
+r = float(input("digite o raio da esfera: "))
+volume = (4/3) * 3.14 * (r ** 3)
+print(f"O volume da esfera com raio {r} é: {volume:.2f}")
