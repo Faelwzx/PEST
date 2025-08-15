ecbfbfc8febd9b3b939fee48e2fa83ea616f0290
Rafael de Oliveira <rafael.marques@aluno.ifce.edu.br>
2025-08-15 13:06:53 -0300

    .

---
+++ lista_2 /q01.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+if numero == 0:
+    print("O número é neutro.")
+elif numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
+    
