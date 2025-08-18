b22bbf58a659a464513dd486aa2902b027ac5a3b
Rafael de Oliveira <rafael.marques@aluno.ifce.edu.br>
2025-08-18 18:24:52 -0300

    .

---
+++ Lista_02/q03.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um numero inteiro: "))
+if numero > 10 and numero < 20:
+    print("maior que 10 ou menor que 20")
+elif numero > 30 and numero < 5:
+    print("maior que 30 ou menor que 5")
+else:
+    print("não é maior que 10 ou menor que 20, nem maior que 30 ou menor que 5")
